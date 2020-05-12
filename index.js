4b3db4e88c399f19c29a534e3a695c6cc56caa61
Zainab Saud <zai99nb@gmail.com>
2020-05-12 14:37:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
+<<<<<<< HEAD
 const companyName = "Scuber";
+=======
+const companyName = "scuber";
+>>>>>>> d5b43d79e95b4936b7f4d81b4301365377fda9ff
 let mostProfitableNeighborhood = "Chelsea";
 let companyCeo = "Susan Smith";
